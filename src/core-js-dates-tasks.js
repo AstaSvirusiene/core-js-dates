83d9e8a709cd18c6ba17999f82f8fa8749d9631e
Asta <asta84@gmail.com>
2024-04-01 21:21:11 +0300

    fix: tests 2

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -109,8 +109,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const dateEnded = new Date(dateEnd).valueOf();
+  const dateStarted = new Date(dateStart).valueOf();
+  const days = (dateEnded - dateStarted) / 86400000 + 1;
+  return days;
 }
 
 /**
